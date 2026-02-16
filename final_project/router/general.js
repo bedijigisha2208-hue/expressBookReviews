b54266d25cef194693c48ef8bf9ea7cc8a0bcc5c
Jigishabedi <bedijigisha2208@gmail.com>
2026-02-16 13:34:01 +0000

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -43,8 +43,27 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  new Promise((resolve, reject) => {
+
+    const book = books[isbn];
+
+    if (book) {
+      resolve(book);
+    } else {
+      reject("Book not found");
+    }
+
+  })
+  .then((data) => {
+    res.status(200).json(data);
+  })
+  .catch((error) => {
+    res.status(404).json({
+      message: error
+    });
+  });
  });
   
 // Get book details based on author
@@ -55,8 +74,25 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  new Promise((resolve,reject) => {
+      const filteredBooks = Object.values(books).filter(
+      (book) => book.title.toLowerCase() === title.toLowerCase()
+  );
+  if(filteredBooks.length > 0) {
+    resolve(filteredBooks)
+  } else {
+    reject("No books found")
+  }
+  })
+  .then((data) => {
+    res.status(200).json(data);
+  })
+  .catch((error) => {
+    res.status(404).json({
+      message: error
+    });
+  });
 });
 
 //  Get book review

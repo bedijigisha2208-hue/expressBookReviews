12427989ea0ea0ace9bae7bf28204c0ce727f494
Jigishabedi <bedijigisha2208@gmail.com>
2026-02-16 14:17:09 +0000

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -68,8 +68,32 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  axios.get("http://localhost:5000/books")
+    .then(function (response) {
+
+      const books = response.data;
+
+      const filteredBooks = Object.values(books).filter(
+        (book) => book.author.toLowerCase() === author.toLowerCase()
+      );
+
+      if (filteredBooks.length > 0) {
+        res.status(200).json(filteredBooks);
+      } else {
+        res.status(404).json({
+          message: "No books found by this author"
+        });
+      }
+
+    })
+    .catch(function (error) {
+      res.status(500).json({
+        message: "Error fetching books",
+        error: error.message
+      });
+    });
 });
 
 // Get all books based on title
